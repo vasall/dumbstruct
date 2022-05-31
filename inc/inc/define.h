a19df598f033a05e7f851cbf3e64cac39f5a1dbf
skratpa <skratpa@gmail.com>
2022-05-31 20:51:38 +0200

    Giving files up

--- inc/inc/define.h
+++
@@ -1,7 +0,0 @@
-#ifndef _DBS_DEFINE_H
-#define _DBS_DEFINE_H
-
-#define DBS_API                extern
-
-
-#endif /* _DBS_DEFINE_H */
--- inc/inc/dumbstruct.h
+++
@@ -1,7 +0,0 @@
-#ifndef _DBS_DUMBSTRUCT_H
-#define _DBS_DUMBSTRUCT_H
-
-#include "define.h"
-#include "christree.h"
-
-#endif /* _DBS_DUMBSTRUCT_H */
